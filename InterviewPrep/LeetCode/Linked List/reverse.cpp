3bdaf4559de1027592c7ae94490e7fa6f20c1c1f
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-05-28 22:07:08 +0530

    206. Reverse Linked List

---
+++ InterviewPrep/LeetCode/Linked List/reverse.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        if(!head || !head->next){
+            return head;
+        }
+        ListNode* curr=head;
+        ListNode* prev=NULL,*Next=head;
+        while(curr){
+            Next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=Next;
+        }
+        return prev;
+    }
+    ListNode* reverseList(ListNode* head) {
+        return reverse(head);
+    }
+};
