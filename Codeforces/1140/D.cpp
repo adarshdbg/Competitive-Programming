10b0e4ba804e9ba7f57419d2e163bfd0c6098b4d
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-08-06 18:52:03 +0530

    D - Minimum Triangulation

---
+++ Codeforces/1140/D.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll  int
+#define vd(T) vector<vector<T>>
+#define v(T) vector<T>
+#define mod (ll)(1e9+7)
+#define pp(T) pair<T,T>
+#define pb push_back
+#define read(x) scanf("%d",&x)
+#define read2(x,y) scanf("%d%d",&x,&y)
+#define read3(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define read4(a,b,c,d) scanf("%d%d%d%d",&a,&b,&c,&d)
+#define mp make_pair
+#define NN 100005
+#define IO ios_base::sync_with_stdio(NULL),cin.tie(0),cout.tie(0)
+#define loop(i,a,b) for(ll i=a;i<b;i++)
+#define DEBUG cout<<"***********DEBUGGING***********"<<endl
+#include <ext/pb_ds/assoc_container.hpp> // Common file 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; 
+ll power(ll a,ll b){ if(b==0) return 1; ll s=power(a,b/2); s*=s;if(b&1) return s*a; return s;}
+ll powermod(ll a,ll b,ll m){ if(b==0) return 1; ll s=power(a,b/2);s*=s; s%=m; if(b&1) return (s*a)%m; return s;}
+inline int add(ll a,ll b,ll m=0){
+    if(m==0) return a+b;
+    return (a%m+b%m)%m;
+}
+ 
+inline int sub(ll a,ll b ,ll m=0){
+    if(m==0) return a-b;
+    return ((a%m-b%m)%m+m)%m;
+}
+ 
+inline int mul(ll a,ll b,ll m=0){
+    if(m==0)return (a*b);
+    return ((a%m)*(b%m))%m;
+}
+ 
+inline int div(ll a,ll b,ll m=0){
+    if(m==0) return a/b;
+    return ((a%m)*(powermod(a,b,m-2)%m))%m;
+}
+ 
+bool isopen(char c){
+    return ((c=='(')||(c=='{')||(c=='['));
+}
+ 
+ll longest_balanced_parenthesis(string a,ll n){
+    stack<char> s;
+    ll start=0;
+    ll ans=0,m=0;
+    for(ll i=0;i<n;i++){
+        if(isopen(a[i])){
+            s.push(a[i]);
+        }
+        else{
+            if(isopen(a[i])|| s.empty()){
+                m=max(m,ans);
+                continue;
+            }
+            else{
+                s.pop();
+                ans+=2;
+                m=max(ans,m);
+            }
+        }
+    }
+ 
+    return m;
+ 
+}
+ 
+ll setup_k(ll *a,ll k){
+  ordered_set x;
+  // Now , insert and then check number of integers less than k by just using x.order_of_key(k+1)
+  return 0;
+}
+ 
+ ll find_area(ll a,ll b,ll c){
+    return a*b*c;
+ }
+ 
+ 
+ ll find_ans(vector<ll> a){
+    if(a.size()<=2)return 0;
+    if(a.size()==3){
+        return find_area(a[0],a[1],a[2]);
+    }
+        vector<ll> v;
+        ll ans=0;
+        ans+=find_area(a[0],a[1],a[2]);
+        v.push_back(a[0]);
+        v.push_back(a[2]);
+        for(ll i=3;i<a.size();i++){
+            v.push_back(a[i]);
+        }
+        ans+=find_ans(v);
+    
+    return ans;
+ 
+ }
+ 
+int main() {
+ 
+    IO; 
+ 
+    ll n;
+    read(n);
+    vector<ll> a;
+    for(ll i=0;i<n;i++){
+        a.push_back(i+1);
+    }
+ 
+    cout<<find_ans(a)<<endl;
+ 
+}
