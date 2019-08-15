59218044c82b9f931388094a336b59c2350e3f05
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-08-16 04:08:20 +0530

    Create pascal'sTriangle2.cpp

---
+++ InterviewPrep/LeetCode/pascal'sTriangle2.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> v;
+        vector<int> s;
+        if(n==0)return v;
+        s.push_back(1);
+        v.push_back(s);
+        
+        for(int i=1;i<n;i++){
+            vector<int> vv;
+            for(int j=0;j<v[v.size()-1].size();j++){
+                vv.push_back(v[v.size()-1][j]+(j?v[v.size()-1][j-1]:0));
+            }
+            vv.push_back(1);
+            v.push_back(vv);
+        } 
+        return v;
+    }
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> v=generate(34);
+        return v[rowIndex];
+    }
+};
