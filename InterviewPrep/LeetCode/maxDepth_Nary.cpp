41138ec7542369e8a50d29f46897f1999d4b906c
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-08-16 02:34:06 +0530

    Create maxDepth_Nary.cpp

---
+++ InterviewPrep/LeetCode/maxDepth_Nary.cpp
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(!root){
+            return 0;
+        }
+        int ans=0;
+        for(auto i:root->children){
+            ans=max(ans,maxDepth(i));
+        }
+        return ans+1;
+    }
+};
