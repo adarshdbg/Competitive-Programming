7ef601eb8163b80182add188d9a311443198164b
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-08-17 18:25:55 +0530

    Generate a random permutation.

---
+++ InterviewPrep/EPI/RandomPermutation.cpp
@@ -0,0 +1,11 @@
+3. Generate a random permutation of a given array and its size. 
+
+class RandomPermutation{
+public:
+	vector<int> ComputeRandomPermutation(int n){
+		vector<int> perm(n,0);
+		iota(permutation.begin(),permutation.end(),0)
+		RandomSampling s;
+		Return s.Solution(perm,n);
+	}
+}; 
